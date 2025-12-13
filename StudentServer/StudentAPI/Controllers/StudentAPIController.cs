7140c3b8cf6e1fafff2fb36ba1f2acbeeac0e9bc
omeed mohammed <omeed1987@gmail.com>
2025-12-13 23:25:39 +0100

    Add GetStudentById Endpoint

--- StudentServer/StudentAPI/Controllers/StudentAPIController.cs
+++ StudentServer/StudentAPI/Controllers/StudentAPIController.cs
@@ -12,19 +12,39 @@ namespace StudentAPI.Controllers
     public class StudentAPIController : ControllerBase
     {
         [HttpGet("All" , Name = "GetAllStudents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
         public ActionResult<IEnumerable<Student>> GetAllStudents()
         {
-            return Ok(StudentDataSimulation.StudentsList);
+            if(StudentDataSimulation.StudentsList.Count == 0)
+                return NotFound("No Students Found!");
+
+            return Ok(StudentDataSimulation.StudentsList); // Returns the List of Students.
         }
 
+
+
         [HttpGet("Passed", Name = "GetPassedStudents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
         public ActionResult<IEnumerable<Student>> GetPassedStudents()
         {
             var passedStudents = StudentDataSimulation.StudentsList.Where(student => student.Grade >= 50).ToList();
+
+            if (passedStudents.Count == 0)
+                return NotFound("No Students Passed!");
+
             return Ok(passedStudents);
         }
 
+
+
         [HttpGet("AverageGrade", Name = "GetAverageGrade")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
         public ActionResult<double> GetAverageGrade()
         {
             if (StudentDataSimulation.StudentsList.Count == 0)
@@ -34,5 +54,25 @@ namespace StudentAPI.Controllers
             return Ok(averageGrade);
         }
 
+
+
+
+        [HttpGet("{id}", Name = "GetStudentById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<Student> GetStudentById(int id)
+        {
+            if (id < 1)
+                return BadRequest($"Not accepted ID {id}");
+
+            var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+                return NotFound($"Student with ID {id} not found.");
+
+            return Ok(student);
+        }
     }
 }
